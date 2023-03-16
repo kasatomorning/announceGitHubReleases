8a90779ef0e4fbfafad52c8a2ccf581cbb49524f
kasatomorning <kasatomo03@gmail.com>
2023-03-16 16:57:58 +0900

    add functions

--- API/api.go
+++ API/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"context"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -13,35 +14,56 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const fileName = "memo.txt"
+const FILENAME = "memo.txt"
 
 func main() {
-	isInit := true
+	// use -i option to initialize file
+	// if not, existing file is used
+	var (
+		i = flag.Bool("i", false, "initialize file")
+	)
+	flag.Parse()
+	isInit := *i
 	if isInit {
+		fmt.Println("initializing file...")
 		//initializing file
-		releases := getReleasesData()
+		releases, getErr := getReleasesData()
+		if getErr != nil {
+			log.Fatal("Error: %v\n", getErr)
+		}
 		initializingErr := initializeFile(releases)
 		if initializingErr != nil {
 			log.Fatal("Error: %v\n", initializingErr)
 		}
 		return
-	} else {
-		//use existing file
-		postreleases, err := readFile(fileName)
-		nowreleases := getReleasesData()
-		writeDataToFile(releases)
+	}
+	//use existing file
+	fmt.Println("loading file...")
+	postreleases, loadingerr := loadFile(FILENAME)
+	if loadingerr != nil {
+		log.Fatal("Error: %v\n", loadingerr)
+	}
+	nowreleases, getErr := getReleasesData()
+	if getErr != nil {
+		log.Fatal("Error: %v\n", getErr)
+	}
+	newData, compareErr := compareAndWriteToFile(postreleases, nowreleases, FILENAME)
+	if compareErr != nil {
+		log.Fatal("Error: %v\n", compareErr)
+	}
+	if len(newData) != 0 {
+		announceDataToXXX(newData)
 	}
 }
 
 func initializeFile(array []string) error {
-	file, err := os.Create(fileName)
+	file, err := os.Create(FILENAME)
 	if err != nil {
-		log.Fatal("Error: Creating file")
 		return err
 	}
 	defer file.Close()
 	for _, line := range array {
-		_, err := file.WriteString(line)
+		_, err := file.WriteString(line + "\n")
 		if err != nil {
 			return err
 		}
@@ -49,10 +71,11 @@ func initializeFile(array []string) error {
 	return nil
 }
 
-func readFile(fn string) ([]string, error) {
+// load file and return the array of string per line and error
+func loadFile(fn string) ([]string, error) {
 	file, err := os.Open(fn)
 	if err != nil {
-		log.Fatal("Error: %v\n", err)
+		return []string{}, err
 	}
 	defer file.Close()
 	returnData := make([]string, 0)
@@ -68,26 +91,53 @@ func readFile(fn string) ([]string, error) {
 		}
 		returnData = append(returnData, string(line))
 	}
+	fmt.Println("Loaded " + fn + " successfully")
 	return returnData, nil
 }
 
-func compareAndWriteToFile(postData []string, nowData []string) error {
-	postData, err := readFile(fn)
-	if err != nil {
-		return err
+func compareAndWriteToFile(postData []string, nowData []string, fn string) ([]string, error) {
+	if len(nowData) == 0 {
+		return []string{}, fmt.Errorf("get empty release array")
 	}
-	for i, line := range array {
-		if line != postData[i] {
-			fmt.Println("New release found: ", line)
+	if len(postData) == 0 {
+		return []string{}, fmt.Errorf("get empty text, please initialize file")
+	}
+	addedData := make([]string, 0)
+	for _, now := range nowData {
+		for j, post := range postData {
+			if now == post {
+				break
+			}
+			if j == len(postData)-1 {
+				addedData = append(addedData, now)
+				fmt.Println("Added release: " + now)
+			}
 		}
 	}
+	if (len(addedData)) == 0 {
+		fmt.Println("No new release")
+		return []string{}, nil
+	}
+	//OpenFile method can overwrite file
+	file, err := os.OpenFile(fn, os.O_WRONLY|os.O_APPEND, 0755)
+	if err != nil {
+		return []string{}, err
+	}
+	defer file.Close()
+	for _, line := range addedData {
+		_, err := file.WriteString(line + "\n")
+		if err != nil {
+			return []string{}, err
+		}
+	}
+	return addedData, nil
 }
 
-func getReleasesData() []string {
-	//initialize .env files
+func getReleasesData() ([]string, error) {
+	//load .env files
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error: Loading .env file")
+		return []string{}, err
 	}
 	token := os.Getenv("GITHUB_TOKEN")
 
@@ -100,35 +150,34 @@ func getReleasesData() []string {
 	releasesList := make([]string, 0)
 
 	for pagecount := 1; ; pagecount++ {
-		//Perpage default is 30
+		//default is 30 -> 100
 		ListOptions := github.ListOptions{PerPage: 100, Page: pagecount}
 		releases, response, err := client.Repositories.ListReleases(ctx, "yt-dlp", "yt-dlp", &ListOptions)
 		//error handling
 		if err != nil {
-			log.Fatal("Error: %v\n", err)
-			return []string{}
+			return []string{}, err
 		}
 		//check response statuscode
 		if response.StatusCode != 200 {
-			log.Fatal("Error: %v\n", response.Status)
-			return []string{}
+			return []string{}, fmt.Errorf("response status_code is not correct")
 		}
 		//check if there are no more releases
 		if len(releases) == 0 {
-			fmt.Println("Reached the last release")
 			break
 		}
 		//check rate limit
 		if response.Rate.Remaining <= 0 {
-			log.Fatal("Error: Rate limit exceeded")
-			return []string{}
+			return []string{}, fmt.Errorf("rate limit exceeded")
 		}
 		for i := 0; i < len(releases); i++ {
 			releasesList = append(releasesList, *releases[i].TagName)
 		}
 	}
-	for i := 0; i < len(releasesList); i++ {
-		fmt.Println(releasesList[i])
-	}
-	return releasesList
+	fmt.Printf("Get %d releases\n", len(releasesList))
+	return releasesList, nil
+}
+
+func announceDataToXXX(data []string) {
+	//to be implemented
+	fmt.Println("なんかに通知する関数")
 }
